dd2fc21be720bc1bb09a1b56660e07e63a8fec5e
dmh5 <denismikulich91@gmail.com>
2024-12-01 19:52:26 +0000

    update

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import os
+
+
+setup(name='freecad.Curves',
+      version="0.0.1",
+      packages=['freecad',
+                'SurfaceMineDesignWB'],
+      maintainer="dmh5",
+      maintainer_email="denismikulich91@gmail.com",
+      url="https://github.com/denismikulich91/SurfaceMineDesignWB",
+      description="Parametric surface mine design tools and more",
+      install_requires=['shapely'],  # should be satisfied by FreeCAD's system dependencies already
+      include_package_data=True)

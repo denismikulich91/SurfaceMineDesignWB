aeeda0f47df376d433a66e8cbe232aa72b1a87fc
Denismikulich91 <denismikulich91@gmail.com>
2024-12-02 09:18:31 +0000

    update

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup
 import os
 
 
-setup(name='freecad.Curves',
+setup(name='SurfaceMineDesign',
       version="0.0.1",
-      packages=['freecad',
-                'SurfaceMineDesignWB'],
+      packages=['SurfaceMineDesignWB'],
       maintainer="dmh5",
       maintainer_email="denismikulich91@gmail.com",
       url="https://github.com/denismikulich91/SurfaceMineDesignWB",

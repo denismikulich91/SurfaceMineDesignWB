d4a309ea224170f4147bcba0b143873f7696c9df
dmh5 <denismikulich91@gmail.com>
2024-12-12 22:15:06 +0000

    from mac

--- InitGui.py
+++ InitGui.py
@@ -11,20 +11,22 @@ from commands.create_optimization_parameters import CreateOptimizationParameters
 # TODO: Make BM a custom feature
 # TODO: give user an option to select datasets to import
 # TODO: Test moving UI Dialogs into Tasks ab
+
+
 class SurfaceMineDesign (Workbench):
     def __init__(self):
         import resources.SurfaceMineDesignerWbIcon as SurfaceMineDesignerWbIcon
         self.__class__.Icon = SurfaceMineDesignerWbIcon.path() + "/workbench.svg"
-        self.__class__.MenuText = "Surface Mine Designer"
+        self.__class__.MenuText = "Surface Mine Design"
 
     def Initialize(self):
         """This function is executed when the workbench is first activated.
         It is executed once in a FreeCAD session followed by the Activated function.
         """
 
-        self.list = ["CreateToe", "CreateCrest", "CreateBench", "CreatePit", "ImportOmf", "PeekOnBlockModel", "CreateOptimizationParameters"]
+        self.list = ["CreateToe", "CreateCrest", "CreateBench", "CreatePit",
+                     "ImportOmf", "PeekOnBlockModel", "CreateOptimizationParameters"]
         self.appendToolbar("SurfaceMineDesignWorkbench", self.list)
-
 
     def Activated(self):
         """This function is executed whenever the workbench is activated"""
@@ -37,11 +39,13 @@ class SurfaceMineDesign (Workbench):
     def ContextMenu(self, recipient):
         """This function is executed whenever the user right-clicks on screen"""
         # "recipient" will be either "view" or "tree"
-        self.appendContextMenu("Surface Mine Design", self.list) # add commands to the context menu
+        # add commands to the context menu
+        self.appendContextMenu("Surface Mine Design", self.list)
 
-    def GetClassName(self): 
+    def GetClassName(self):
         # This function is mandatory if this is a full Python workbench
         # This is not a template, the returned string should be exactly "Gui::PythonWorkbench"
         return "Gui::PythonWorkbench"
+
 
 Gui.addWorkbench(SurfaceMineDesign())
